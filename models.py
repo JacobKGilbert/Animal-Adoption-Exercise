5241201749d672bd10467ab042b55473c39cd2c7
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-05-02 08:09:52 -0500

    created Pet model

---
+++ models.py
@@ -0,0 +1,22 @@
+'''Models for Adoption'''
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+  db.app = app
+  db.init_app(app)
+
+class Pet(db.Model):
+  '''Pet Model'''
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  name = db.Column(db.Text, nullable=False)
+  species = db.Column(db.Text, nullable=False)
+  photo_url = db.Column(db.Text)
+  age = db.Column(db.Integer)
+  notes = db.Column(db.Text)
+  available = db.Column(db.boolean, nullable=False, default=True)
+
+  def __repr__(self):
+    p = self
+    return f'< ID={p.id} Name={p.name}, Species={p.species} >'
